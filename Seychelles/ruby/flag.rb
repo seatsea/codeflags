6989fde905d09f8b76e0d9d2ad0f81deb9e38d60
Robin Broda <robin@broda.me>
2016-03-06 21:50:05 +0100

    added Seychelles in ruby

---
+++ Seychelles/ruby/flag.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+require "colorize"
+
+def b(c,a=1)
+  return "▮".colorize(:color => c, :background => c)*a
+end
+
+width = 80
+height = width/2
+height.times do |heightc|
+  width.times do |widthc|
+    if widthc*3+heightc*2<=width
+      print b(:blue)
+    elsif widthc*1.5+heightc*2<=width
+      print b(:yellow)
+    elsif widthc/1.5+heightc*2<=width
+      print b(:red)
+    elsif widthc/3+heightc*2<=width
+      print b(:white)
+    else
+     print b(:green)
+    end
+  end
+  puts
+end
+puts b(:blue)+b(:green, width-1)
