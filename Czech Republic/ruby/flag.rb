12c68a5558a4f49fe3bed097309619b48804b641
Robin Broda <robin@broda.me>
2016-03-06 16:52:31 +0100

    added Czech Republic in ruby

---
+++ Czech Republic/ruby/flag.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+require "colorize"
+
+def b(c,a=1)
+  return "▮".colorize(:color => c, :background => c)*a
+end
+
+width = 80
+arrow = width/2
+arrowc = 0
+(arrow/2).times do
+  puts b(:blue, arrowc+2) + b(:white, width-(arrowc+2))
+  arrowc = arrowc+2
+  break if arrowc == arrow
+end
+(arrow/2).times do
+  puts b(:blue, arrowc) + b(:red, width-arrowc)
+  arrowc = arrowc-2
+  break if arrowc == 0
+end
