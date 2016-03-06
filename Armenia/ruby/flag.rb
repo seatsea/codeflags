945df1ed7c02fb6823786fb508fb2c1f0e956869
Robin Broda <robin@broda.me>
2016-03-06 17:31:45 +0100

    added Armenia in ruby

---
+++ Armenia/ruby/flag.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+require "colorize"
+
+def b(c,a=1)
+  return "▮".colorize(:color => c, :background => c)*a
+end
+
+width = 80
+length = 30
+stripe = length/3
+
+3.times do |time|
+  color = ""  
+  case time
+  when 0
+    color = :red
+  when 1
+    color = :blue
+  when 2
+    color = :yellow
+  end
+  stripe.times do
+    puts b(color, width)
+  end
+end
