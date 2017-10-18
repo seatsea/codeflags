18bee39d8604da934591fe14edc499b31f5e4921
Dominik Kwiatek <moltenheim@gmail.com>
2017-10-18 22:37:40 +0100

    Added England

---
+++ England/ruby/flag.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+require "colorize"
+
+#default is 22
+length = 80
+
+def b(c,a=1)
+  return "▮".colorize(:color => c, :background => c)*a
+end
+
+def tb(size)
+	section_length = size/5
+	10.times do
+		print b(:white, section_length*2)
+		print b(:red, section_length)
+		puts b(:white, section_length*2)
+	end
+end
+
+tb(length)
+6.times { puts b(:red, length) }
+tb(length)
