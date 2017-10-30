002ab2415db7693e25bbca81af107b3ba944551a
Andreas N <git@pixelde.su>
2017-10-30 23:38:48 +0100

    Add Colombian Department of Antioquia's flag (#24)
    * Add Antioquia flag
    
    * Antioquia -> Colombian Department of Antioquia

---
+++ Colombian Department of Antioquia/ruby/flag.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+require "colorize"
+
+def b(c,a=1)
+  return "▮".colorize(:color => c, :background => c)*a
+end
+
+width = 80
+length = 30
+stripe = length/2
+
+2.times do |time|
+  color = ""  
+  case time
+  when 0
+    color = :white
+  when 1
+    color = :green
+  end
+  stripe.times do
+    puts b(color, width)
+  end
+end
