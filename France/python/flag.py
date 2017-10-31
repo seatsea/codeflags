1eaf5065afd6864f0603ad7c490e0d9bfca73e67
seatsea <seatsea@seatsea.eu>
2017-10-31 01:49:08 +0100

    Or

--- France/python/flag.py
+++ France/python/flag.py
@@ -1,6 +1,5 @@
 import math
 
-
 END = "\033[0m"
 RED = "\033[31;41m"
 WHITE = "\033[37;47m"
@@ -16,11 +15,11 @@ total = ""
 
 for i in range(0, height):
     color_id = math.ceil((i+1/stripe_h) % stripe_h)
-    if color_id == 1:
+    if color_id == 0 or 1:
         colors = BLUE
     elif color_id == 2:
         colors = WHITE
     elif color_id == 3:
         colors = RED
 
-    print(color+"▮", end="")
+    print((color+"a")*width)
