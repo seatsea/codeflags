3564259307608d425d23db6dddaef5e4b277178b
seatsea <seatsea@seatsea.eu>
2017-10-31 01:41:51 +0100

    Getting there

--- France/python/flag.py
+++ France/python/flag.py
@@ -1,8 +1,10 @@
-class colors:
-    END = "\033[0m"
-    RED = "\033[31;41m"
-    WHITE = "\033[37;47m"
-    BLUE = "\033[34;44m"
+import math
+
+
+END = "\033[0m"
+RED = "\033[31;41m"
+WHITE = "\033[37;47m"
+BLUE = "\033[34;44m"
 
 width = 80
 height = 30
@@ -13,11 +15,12 @@ total = ""
 
 
 for i in range(0, height):
-    
-	total += colors.BLUE + "B" + colors.END
-	total += colors.WHITE + "W" + colors.END
-    total += colors.RED + "R" + colors.END
+    color_id = math.ceil((i+1/stripe_h) % stripe_h)
+    if color_id == 1:
+        colors = BLUE
+    elif color_id == 2:
+        colors = WHITE
+    elif color_id == 3:
+        colors = RED
 
-    total += "\n"
-
-print(total)
+    print(color+"▮", end="")
